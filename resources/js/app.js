8cdeac0c52f8c1c7f81a53940de1e66b340685cb
Modul <modul@mail.ru>
2023-02-12 20:38:09 +0400

    Add Element-ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './core/App.vue'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+Vue.use(ElementUI, { locale })
 
 new Vue({
     el: '#app',
--- resources/js/core/App.vue
+++ resources/js/core/App.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>App</h1>
+    <div>
+        <el-button type="text" @click="centerDialogVisible = true">Click to open the Dialog</el-button>
+
+        <el-dialog title="Warning" :visible.sync="centerDialogVisible" width="30%" center>
+            <span>It should be noted that the content will not be aligned in center by
+                default</span>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="centerDialogVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="centerDialogVisible = false">Confirm</el-button>
+            </span>
+        </el-dialog>
+    </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            centerDialogVisible: false,
+        };
+    },
+};
+</script>
